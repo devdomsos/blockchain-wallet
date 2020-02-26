0d4350174269306c2918dc1bb6ebd69d6b5eee78
Dom Sos <devdomsos@gmail.com>
2020-02-26 14:36:26 +0100

    Added DaiTokenMock and minted 1000 tokens

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+
+  await tokenMock.mint(
+    '0x08599F17dA865Af2656533e50712CcEBe4C4E646',
+    '1000000000000000000000'
+  )
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import Web3 from 'web3';
 
 class App extends Component {
+
+  async componentWillMount () {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-ethereum browser detected. You should consider trying a MetaMask account')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({ account: accounts[0]})
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { };
+  
+  }
+
+
+
   render() {
     return (
       <div>
